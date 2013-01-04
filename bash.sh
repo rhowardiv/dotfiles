fd3c4a49f646573c301733c02c80f920cd1eee97
Richard Howard <rh@richardhoward.net>
2013-01-04 10:13:58 -0500

    Flush every bash command to history immediately

--- bash.sh
+++ bash.sh
@@ -1,6 +1,8 @@
 export HISTSIZE=10000
 export PS1=$'\[\e[32m\][\[\e[31m\]\u@\H\[\e[34m\] \[\e[35m\]\A \[\e[33m\]\w\[\e[32m\]]\$ \[\e[0m\]'
 export EDITOR=vim
+# flush every command to history file immediately
+export PROMPT_COMMAND='history -a'
 
 PATH_HAS_HOME_BIN=$(echo "$PATH" | grep "\(^\|:\)$HOME/bin\($\|:\)")
 if [ -z "$PATH_HAS_HOME_BIN" ]; then
