b2d6d46300e744200ab4fdab9a740325f257d182
Richard Howard <rh@richardhoward.net>
2013-02-07 10:28:01 -0500

    Suppress stderr from which vcprompt

--- bash.sh
+++ bash.sh
@@ -1,6 +1,6 @@
 export HISTSIZE=10000
 export PS1=$'\[\e[32m\][\[\e[31m\]\u@\H\[\e[34m\] \[\e[35m\]\A \[\e[33m\]\w\[\e[32m\]]\$ \[\e[0m\]'
-if [ -n "$(which vcprompt)" ]; then
+if [ -n "$(which vcprompt 2>/dev/null)" ]; then
 	export VCPROMPT_FORMAT=" %b%m%u"
 	export PS1=$'\[\e[32m\][\[\e[31m\]\u@\H\[\e[34m\] \[\e[35m\]\A \[\e[33m\]\w\[\e[36m\]$(vcprompt)\[\e[32m\]]\$ \[\e[0m\]'
 fi
